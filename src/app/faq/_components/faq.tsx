a109bf213051d47165afa805abfe118afda99c5c
almostJohn <garcia.johngale@gmail.com>
2024-05-12 11:05:31 +0800

    add: faq page

---
+++ src/app/faq/_components/faq.tsx
@@ -0,0 +1,102 @@
+import * as React from "react";
+import { cn } from "~/util/cn";
+
+export function FAQ() {
+	return (
+		<>
+			<div
+				className={cn(
+					"collapse collapse-arrow border border-neutral-300 bg-white",
+					"rounded-lg",
+				)}
+			>
+				<input type="radio" name="faq-accordion" defaultChecked />
+				<div className="collapse-title text-lg font-bold leading-none tracking-tight">
+					What are the limits of the Free plan?
+				</div>
+				<div className="collapse-content">
+					<p className="text-sm leading-4">
+						The free plan gives you access to basic task management. However,
+						you cannot create tasks in the same way as in the Premium plan.
+					</p>
+				</div>
+			</div>
+			<div
+				className={cn(
+					"collapse collapse-arrow border border-neutral-300 bg-white",
+					"rounded-lg",
+				)}
+			>
+				<input type="radio" name="faq-accordion" />
+				<div className="collapse-title text-lg font-bold leading-none tracking-tight">
+					What&apos;s included in the Premium plan?
+				</div>
+				<div className="collapse-content">
+					<p className="text-sm leading-4">
+						The premium plan gives access to the task templates and advanced
+						task management features.
+					</p>
+				</div>
+			</div>
+			<div
+				className={cn(
+					"collapse collapse-arrow border border-neutral-300 bg-white",
+					"rounded-lg",
+				)}
+			>
+				<input type="radio" name="faq-accordion" />
+				<div className="collapse-title text-lg font-bold leading-none tracking-tight">
+					Is tasktrove&apos;s platform secure for sharing sensitive information?
+				</div>
+				<div className="collapse-content">
+					<p className="text-sm leading-4">
+						Yes, tasktrove takes the security and confidentiality of your
+						information very seriously. The platform employs industry-standard
+						encryption protocols to protect your data and restricts access to
+						authorized personnel only. You can trust tasktrove with your
+						sensitive tasks and information.
+					</p>
+				</div>
+			</div>
+			<div
+				className={cn(
+					"collapse collapse-arrow border border-neutral-300 bg-white",
+					"rounded-lg",
+				)}
+			>
+				<input type="radio" name="faq-accordion" />
+				<div className="collapse-title text-lg font-bold leading-none tracking-tight">
+					How does tasktrove handle deadlines and turnaround times?
+				</div>
+				<div className="collapse-content">
+					<p className="text-sm leading-4">
+						tasktrove understands the importance of deadlines and works
+						diligently to meet them. When you submit a task, you can specify
+						your deadline, and tasktrove will do its best to deliver the
+						completed task within that timeframe. If there are any unforeseen
+						delays, you will be notified promptly, and alternative arrangements
+						will be made if necessary. Your time is valuable, and tasktrove is
+						dedicated to helping you meet your goals on schedule.
+					</p>
+				</div>
+			</div>
+			<div
+				className={cn(
+					"collapse collapse-arrow border border-neutral-300 bg-white",
+					"rounded-lg",
+				)}
+			>
+				<input type="radio" name="faq-accordion" />
+				<div className="collapse-title text-lg font-bold leading-none tracking-tight">
+					What payment method do tasktrove accept?
+				</div>
+				<div className="collapse-content">
+					<p className="text-sm leading-4">
+						We accept credit cards and PayPal. In the future, we will be more
+						open to other payment methods for other countries to adapt to.
+					</p>
+				</div>
+			</div>
+		</>
+	);
+}
---
+++ src/app/faq/page.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { FAQ } from "./_components/faq";
+
+export default function Page() {
+	return (
+		<>
+			<div className="flex flex-col items-center justify-center space-y-6">
+				<div className="text-center">
+					<h1 className="text-3xl font-bold leading-none tracking-tighter md:text-5xl">
+						Frequently Asked Question
+					</h1>
+					<p className="py-6 text-lg font-light text-neutral-500">
+						Find answers to common questions about our website/app, simplifying
+						your queries for a smoother experience.
+					</p>
+				</div>
+				<div className="flex flex-col items-center justify-center gap-4">
+					<FAQ />
+				</div>
+			</div>
+		</>
+	);
+}
